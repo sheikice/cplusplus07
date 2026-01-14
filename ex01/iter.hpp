25bb0d67d61d5e56cad08be7fe9630a0f61ae423
Jeremy Wuille <jwuille@f1br8s10.clusters.42paris.fr>
2026-01-14 12:26:02 +0100

    [fix/ex01] - better template func + more tests

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -4,8 +4,8 @@
 #include <cstddef>
 #include <iostream>
 
-template <typename T>
-void iter(const T* arr, const size_t len, void (*function)(const T&))
+template <typename T, typename FUNC>
+void iter(T* arr, const size_t len, FUNC func)
 {
 	if (!arr)
 	{
@@ -13,16 +13,6 @@ void iter(const T* arr, const size_t len, void (*function)(const T&))
 		return ;
 	}
 	for (size_t i = 0; i < len; i++)
-		function(arr[i]);
+		func(arr[i]);
 }
-
-template <typename T>
-void iter(T* arr, const size_t len, void (*function)(T&))
-{
-	if (!arr || len <= 0)
-		return ;
-	for (size_t i = 0; i < len; i++)
-		function(arr[i]);
-}
-
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 
 template <typename T>
-void show(const T& val)
+void showArray(const T& val)
 {
     std::cout << val << '\n';
 }
@@ -14,21 +14,26 @@ void	reset(T& val)
 	val = 0;
 }
 
+void	showIntArray(int val)
+{
+	std::cout << val << '\n';
+}
+
 int main(void)
 {
 	const size_t size = 5;
 	std::string array[size] = {"first", "second", "third", "fourth", "fifth"};
 	int tab[size] = {4, 3, 2, 1, 0};
 
-	std::cout << "[TEST 1: show string array]\n";
-	iter(array, size, show);
+	std::cout << "\n[TEST 1: show string array]\n";
+	iter(array, size, showArray<std::string>);
 
-	std::cout << "[TEST 2: show int array before and after reset to 0]\n";
+	std::cout << "\n[TEST 2: show int array before and after reset to 0]\n";
 	std::cout << "Before:\n";
-	iter(tab, size, show);
-	iter(tab, size, reset);
+	iter(tab, size, showArray<int>);
+	iter(tab, size, reset<int>);
 	std::cout << "After:\n";
-	iter(tab, size, show);
+	iter(tab, size, showIntArray);
 	std::cout << "\n";
 
 	return 0;
